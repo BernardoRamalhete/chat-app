866f376fe105cb1bf0869feac705387775b5a3e4
Bernardo Ramalhete <Bernardo>
2022-03-26 08:11:59 -0300

    fixed envs

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
 
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID
-  };
+    apiKey: `${process.env.NEXT_PUBLIC_API_KEY}`,
+    authDomain: `${process.env.AUTH_DOMAIN}`,
+    projectId: `${process.env.PROJECT_ID}`,
+    storageBucket: `${process.env.STORAGE_BUCKET}`,
+    messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
+    appId: `${process.env.APP_ID}`
+};
 
 const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 
